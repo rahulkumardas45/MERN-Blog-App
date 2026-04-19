cd8ff238a4fb6659b30c05bfc9c377ccade0fe37
rahulkumar <user.email.like:"rahulku3223@gmail.com>
2026-04-20 00:52:42 +0530

    "fix redis connection"

--- Api/config/redis.js
+++ Api/config/redis.js
@@ -1,21 +1,15 @@
 import { createClient } from "redis";
 
 const client = createClient({
-  username: "default",
-  password: process.env.REDIS_PASSWORD, 
-  socket: {
-    host: "redis-16331.crce286.ap-south-1-1.ec2.cloud.redislabs.com",
-    port: 16331
-  }
+  url: process.env.REDIS_URL
 });
-
 
 client.on("error", (err) => console.log("Redis Client Error:", err));
 
 export const connectRedis = async () => {
   try {
-    await client.connect();  
-    console.log(" Redis connected");
+    await client.connect();
+    console.log("Redis connected");
   } catch (err) {
     console.log("❌ Redis not running");
   }
